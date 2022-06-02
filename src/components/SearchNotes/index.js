8d1c6012c38a26e0a76384c7568991abfe04568c
Diego Rocha <dkxever@gmail.com>
2022-06-02 20:53:07 -0300

    Creating an annotation search component

--- src/components/SearchNotes/index.js
+++ src/components/SearchNotes/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { MdSearch } from 'react-icons/md'
 
 const SearchNotes = () => {
   return (
-    <div>
-
+    <div className="search">
+      {/* ícone de busca */}
+      <MdSearch size="1.2em" className="search-icon" />
+      <input 
+        type="text" 
+        placeholder="Faça a sua pesquisa..." 
+      />
     </div>
   )
 }
