5c1894539c4be248e4b19f6032b421727a309f5d
Diego Rocha <dkxever@gmail.com>
2022-06-02 20:50:13 -0300

    Creating handleChange and handleClickSave in add new annotations and note

--- src/components/AddNotes/index.js
+++ src/components/AddNotes/index.js
@@ -1,20 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 // Componete para adicionar novas anotações
 
 const AddNotes = ({ handleAddNotes }) => {
+
+  // Texto de nova anotação a ser adicionada
+
+  const [ noteText, setNoteText ] = useState('');
+
+  // Quantidade limite de caracteres
+  const characterLimit = 200;
+  
+  /* se a quantidade de caracteres for maior ou igual á 0, atualiza nosso estado de acordo com o que estamos digitando */
+  const handleChange = ({ target }) => {
+    if(characterLimit - target.value.length >= 0){
+      setNoteText(target.value);
+    }
+  }
+  
+  /* envia se eu tiver algum texto e após o envio limpar o estado */
+  const handleSaveClick = () => {
+    if(noteText.trim().length > 0){
+      handleAddNotes(noteText);
+      setNoteText('');
+    }
+  }
 
   return (
     <div className="note new">
       <textarea
         cols="8"
         rows="10"
+        value={noteText}
+        onChange={handleChange}
         placeholder="Digite uma nova anotação..."
       >
       </textarea>
       <div className="note-footer">
-        <small>Characteres</small>
-        <button className="save">Salvar</button>
+        <small>{characterLimit - noteText.length} Characteres</small>
+        <button className="save" onClick={handleSaveClick}>Salvar</button>
       </div>
     </div>
   )
