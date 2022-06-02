1b2e7f231c611eeeebf92b52e0d506d1fe0a8a19
Diego Rocha <dkxever@gmail.com>
2022-06-02 20:36:49 -0300

    Creating component to add new annotations

--- src/components/AddNotes/index.js
+++ src/components/AddNotes/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
 const AddNotes = () => {
-  <div>
-    
-  </div>
+  return (
+    <div className="note new">
+      <textarea
+        cols="8"
+        rows="10"
+        value={}
+        onChange={}
+        placeholder="Digite uma nova anotação..."
+      >
+      </textarea>
+      <div className="note-footer">
+        <small>Characteres</small>
+        <button className="save">Salvar</button>
+      </div>
+    </div>
+  )
 }
 
 export default AddNotes
