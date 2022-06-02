b6f67399a5bcaac39e5e0465bb37f3ccf724f4bb
Diego Rocha <dkxever@gmail.com>
2022-06-02 19:52:15 -0300

    Create initial structures of components required for the application

---
+++ src/components/AddNotes/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddNotes = () => {
+  <div>
+    
+  </div>
+}
+
+export default AddNotes
---
+++ src/components/SearchNotes/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchNotes = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default SearchNotes
